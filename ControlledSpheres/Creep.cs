a1d27407954a20c52973f54427d18edb7b582e39
William Bennett <williambennett2014@gmail.com>
2016-02-25 20:28:36 -0800

    Added Tower and Creep classes

--- ControlledSpheres/Creep.cs
+++ ControlledSpheres/Creep.cs
@@ -7,13 +7,27 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace ControlledSpheres {
-    class Creep : AnimatedGameObject {
+    public class Creep : AnimatedGameObject {
         int HP;
-        public Creep(Animation[] animationList, Vector3 position, int HP)
+        public Creep(Animation[] animationList, Vector2 position, int HP)
             : base(animationList, position) {
             this.HP = HP;
         }
 
+        public Creep(Animation[] animationlist, Vector2 position, Vector2 velocity, int HP)
+            : base(animationlist, position, velocity) {
+            this.HP = HP;
+        }
+
+        public Creep(Animation animation, Vector2 position, int HP)
+            : base(animation, position) {
+            this.HP = HP;
+        }
+
+        public Creep(Animation animation, Vector2 position, Vector2 velocity, int HP)
+            : base(animation, position, velocity) {
+            this.HP = HP;
+        }
         public override void Update(GameTime gameTime) {
             base.Update(gameTime);
         }
---
+++ ControlledSpheres/Tower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ControlledSpheres {
+
+    public interface GameEntity {
+
+    }
+
+    public class Tower : AnimatedGameObject {
+        public Tower(Animation[] animation, Vector2 position)
+            : base(animation, position) {
+                ;
+        }
+
+        public Tower(Animation animation, Vector2 position)
+            : base(animation, position) {
+                ;
+        }
+
+        
+    }
+}
