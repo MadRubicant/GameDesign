b2ea1504f4c24575f822c55b8e86366dcb887bf5
William Bennett <williambennett2014@gmail.com>
2016-03-07 18:58:00 -0800

    Added more extension methods, including rotation and angles

--- ControlledSpheres/ExtensionMethods.cs
+++ ControlledSpheres/ExtensionMethods.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace ExtensionMethods {
     public static class ExtensionMethods {
+        static float FloatEpsilon = (float)Math.Sin(Math.PI);
         // Returns a Vector2 with the X and Y component of the current Vector3
         public static Vector2 ToVector2(this Vector3 vector) {
             Vector2 ret = new Vector2(vector.X, vector.Y);
@@ -39,5 +40,37 @@ namespace ExtensionMethods {
             return angle;
         }
 
+        /// <summary>
+        /// Rotates the current vector to point in the same direction as the Other parameter
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="Other"></param>
+        /// <returns></returns>
+        public static Vector2 Rotate(this Vector2 vector, Vector2 Other) {
+            Other.Normalize();
+            Other *= vector.Length();
+            return Other;
+        }
+
+        public static Vector2 Rotate(this Vector2 vector, float Angle) {
+            float InitialAngle = vector.Angle();
+            float NewAngle = InitialAngle + Angle;
+            return Vector2.Zero;
+        }
+
+        public static bool FuzzyEqual(this Single flt, float other) {
+            float diff = Math.Abs(flt - other);
+            float relativeDiff = FloatEpsilon * Math.Max(Math.Abs(flt), Math.Abs(other));
+            Console.WriteLine("{0} <= {1}", diff, relativeDiff);
+            if (diff <= relativeDiff)
+                return true;
+            else return false;
+        }
+
+        public static bool FuzzyEqual(this Vector2 vector, Vector2 other) {
+            if (vector.X.FuzzyEqual(other.X) && vector.Y.FuzzyEqual(other.Y))
+                return true;
+            else return false;
+        }
     }
 }
