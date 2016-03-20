f4017f69502c9ee05c71baa80fe0b4ade654b003
William Bennett <williambennett2014@gmail.com>
2016-03-19 19:42:09 -0700

    Began work on Projectile class. Projectiles target a GameObject; to have a fixed target, create an empty game object with a specific position and no velocity

---
+++ ControlledSpheres/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using ExtensionMethods;
+
+using ControlledSpheres.Graphics;
+namespace ControlledSpheres {
+    public class Projectile : AnimatedGameObject {
+        public GameObject Target { get; set; }
+        public Projectile(Animation animation, Vector2 Position, Vector2 Velocity, GameObject Target) :
+            base(animation, Position, Velocity) {
+                this.Target = Target;
+                RotMode = RotationMode.Velocity;
+        }
+
+        public override void Update(GameTime gameTime) {
+            Vector2 Direction = Target.Center - this.Center;
+            Velocity = Velocity.Rotate(Direction);
+            base.Update(gameTime);
+        }
+
+
+    }
+
+    
+}
