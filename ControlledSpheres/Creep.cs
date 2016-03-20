9ba16cc21fee7198756f23418da79c223df23087
William Bennett <williambennett2014@gmail.com>
2016-03-19 19:39:58 -0700

    Added some rotation code to Creep class

--- ControlledSpheres/Creep.cs
+++ ControlledSpheres/Creep.cs
@@ -6,29 +6,32 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
+using ControlledSpheres.Graphics;
 namespace ControlledSpheres {
     public class Creep : AnimatedGameObject {
-        int HP;
-
-        
+        public int HP { get; set; }
         public Creep(Animation[] animationList, Vector2 position, int HP)
             : base(animationList, position) {
             this.HP = HP;
+            RotMode = RotationMode.Velocity;
         }
 
         public Creep(Animation[] animationlist, Vector2 position, Vector2 velocity, int HP)
             : base(animationlist, position, velocity) {
             this.HP = HP;
+            RotMode = RotationMode.Velocity;
         }
 
         public Creep(Animation animation, Vector2 position, int HP)
             : base(animation, position) {
             this.HP = HP;
+            RotMode = RotationMode.Velocity;
         }
 
         public Creep(Animation animation, Vector2 position, Vector2 velocity, int HP)
             : base(animation, position, velocity) {
             this.HP = HP;
+            RotMode = RotationMode.Velocity;
         }
 
         public override void Update(GameTime gameTime) {
